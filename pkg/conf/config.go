05a55860c96f7c5c3bacb0b77b3912af4ba2d18d
Nikolay Bondarenko <misterionkell@gmail.com>
2018-11-27 21:07:32 +0300

    Add config and logging module

---
+++ pkg/conf/config.go
@@ -0,0 +1,70 @@
+package conf
+
+import (
+	"crypto/rsa"
+	"strings"
+
+	//"github.com/benoitmasson/viper"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type ServerConfig struct {
+	Port int
+}
+
+type Database struct {
+	Host     string
+	Database string
+	User     string
+	Password string
+}
+
+type Jwt struct {
+	SignatureSecret       *rsa.PublicKey
+	SignatureSecretBase64 string
+	Algorithm             string
+}
+
+type GeoIP struct {
+	DBPath string
+}
+
+// Config the application's configuration
+type Config struct {
+	Server    ServerConfig
+	Database  Database
+	Jwt       Jwt
+	GeoIP     GeoIP
+	LogConfig LoggingConfig
+}
+
+// LoadConfig loads the config from a file if specified, otherwise from the environment
+func LoadConfig(cmd *cobra.Command) (*Config, error) {
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return nil, err
+	}
+
+	viper.SetEnvPrefix("QILIN")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	if configFile, _ := cmd.Flags().GetString("config"); configFile != "" {
+		viper.SetConfigFile(configFile)
+	} else {
+		viper.SetConfigName("config")
+		viper.AddConfigPath("./")
+		viper.AddConfigPath("$HOME/.example")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := new(Config)
+	if err := viper.Unmarshal(config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
