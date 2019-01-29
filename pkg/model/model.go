c8975974b0e8fa2ed0f8d549d12b46538bd77e94
Roman Golenok <roman.golenok@gmail.com>
2019-01-29 13:40:45 +0300

    [QILIN-33] pointer for UpdatedAt removed

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -13,6 +13,6 @@ import (
 type Model struct {
 	ID        uuid.UUID `gorm:"type:uuid; primary_key"`
 	CreatedAt time.Time
-	UpdatedAt *time.Time
+	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
 }
